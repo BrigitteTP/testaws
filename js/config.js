91225328941525ecfce7254b936d45cb7f255de7
BrigitteTP <43347556+BrigitteTP@users.noreply.github.com>
2023-03-14 21:20:05 -0500

    update

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId:'us-east-1_fnlPG5MuK', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1dr8sgks2nr2dbvjstikor1qea', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
